635dee5de805951f4f988bb94d43e852bd88685e
Алексей Тихончук <alexeytikhonchuk@gmail.com>
2022-03-26 15:55:27 +0300

    Перед работой в новой PyCharm

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='main_page'),
     path('group/<slug:slug>/', views.group_posts),
+    path('group_list.html', views.group_list, name='group_list')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
-    template = 'posts/index.html'
-    return render(request, template)
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Записи по теме {slug}')
+    return HttpResponse(f'Запись {slug}')
